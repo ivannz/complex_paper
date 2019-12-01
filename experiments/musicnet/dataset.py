6f55916f6e63a937f010bf1155262a9828aad8e0
ivannz <ivannz@yandex.ru>
2019-12-01 19:22:06 +0300

    moved reasmpling to a special script, since it is a one-time procedure

--- experiments/musicnet/dataset.py
+++ experiments/musicnet/dataset.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import tqdm
 import h5py
 import numpy as np
 
@@ -9,42 +5,9 @@ import torch
 import torch.utils.data
 
 from ncls import NCLS64
-from resampy import resample
 from bisect import bisect_right
 
 from numpy.lib.stride_tricks import as_strided
-
-
-def resample_h5(file_in, file_out, frame_rate_in, frame_rate_out, keys=None):
-    # Authors: Dmitriy Serdyuk
-    # (2019-11-24) changed to hdf5 supports
-    print(f""".. resampling {file_in} ({frame_rate_in}Hz) """
-          f"""into {file_out} ({frame_rate_out}Hz)""", flush=True)
-
-    with h5py.File(file_in, "r") as h5_in, \
-            h5py.File(file_out, "w") as h5_out:
-
-        ratio = frame_rate_out / float(frame_rate_in)
-
-        # pour from h5_in to h5_out
-        items = h5_in.items()
-        if keys is not None:
-            items = [(k, h5_in[k]) for k in keys]
-
-        for key, group in tqdm.tqdm(items):
-
-            # resample the waveform
-            data = resample(np.array(group["data"]),
-                            frame_rate_in, frame_rate_out)
-            h5_out.create_dataset(f"{key}/data", data=data)
-
-            # change start-end times
-            labels = np.array(group["labels"])
-            labels["start_time"] = (labels["start_time"] * ratio).astype(int)
-            labels["end_time"] = (labels["end_time"] * ratio).astype(int)
-            h5_out.create_dataset(f"{key}/labels", data=labels)
-
-    return file_out
 
 
 class MusicNetHDF5(torch.utils.data.Dataset):
