db3706b578138abaa856b64ee23e1e4005b354df
ivannz <ivannz@yandex.ru>
2019-12-01 22:12:32 +0300

    partial scaffolding of the experiment runner

--- experiments/auto/auto.py
+++ experiments/auto/auto.py
@@ -1,3 +1,4 @@
+__version__ = "0.1"
 import os
 import copy
 import tqdm
@@ -8,9 +9,116 @@ import torch
 import numpy as np
 
 from setuptools._vendor.packaging.version import Version
+from .utils import feed_limiter, feed_mover
 
 
-__version__ = "0.1"
+def defaults(options):
+    # currently no defaults
+    return copy.deepcopy(options)
+
+
+def get_datasets(factory, recipe):
+    """Get dataset instances from the factory specification and recipe."""
+    # "dataset", "dataset_sources"
+    raise NotImplementedError
+
+
+def get_collate_fn(recipe):
+    """Get collation function responsible for batch-feature generation."""
+    # "features"
+    raise NotImplementedError
+
+
+def get_feeds(datasets, collate_fn, recipe):
+    """Get instances of data loaders from the datasets and collate function."""
+    # <datasets>, <collate_fn>, "feeds"
+    raise NotImplementedError
+
+
+def get_objective_terms(recipe):
+    """Gather the components of the objective function."""
+    # "objective_terms"
+    raise NotImplementedError
+
+
+def get_model_factory(recipe):
+    """Partial constructor of the model. Assumes a particual common API."""
+    # "model"
+    raise NotImplementedError
+
+
+def get_model(factory, recipe):
+    """Partial constructor of the model. Assumes a particual common API."""
+    # <factory>, "stages__*__model"
+    raise NotImplementedError
+
+
+def get_objective(objective_terms, recipe):
+    """Construct the objective function from the recipe and components."""
+    # <objective_terms>, "stages__*__objective"
+    raise NotImplementedError
+
+
+def get_optimizer(module, *, lr, **kwargs):
+    """Get the optimizer for the module from explicit lr and kwargs."""
+    raise NotImplementedError
+
+
+def get_scheduler(optimizer, **kwargs):
+    """Get scheduler for the optimizer and kwargs."""
+    raise NotImplementedError
+
+
+class FeedWrapper(object):
+    def __init__(self, feed, max=1000, **kwargs):
+        self.feed, self.max, self.kwargs = feed, max, kwargs
+
+    def __iter__(self):
+        feed = feed_limiter(self.feed, self.max)
+        yield from feed_mover(feed, **self.kwargs)
+
+    def __len__(self):
+        return len(self.feed)
+
+
+def run(options, verbose=True):
+    options = defaults(options)
+
+    # placement (dtype / device) and sparsity settings
+    threshold = options["threshold"]  # log(p/(1-p)), p=dropout rate
+    devtype = dict(device=torch.device(options["device"]), dtype=torch.float32)
+
+    datasets = get_datasets(options["dataset"], options["dataset_sources"])
+
+    collate_fn = get_collate_fn(options["features"])
+
+    feeds = get_feeds(datasets, collate_fn, options["feeds"])
+
+    objective_terms = get_objective_terms(options["objective_terms"])
+
+    model_factory = get_model_factory(options["model"])
+
+    model, optim = None, None
+    for stage, settings in options["stages"].itmes():
+        new_model = get_model(model_factory, settings["model"]).to(**devtype)
+        new_optim = get_optimizer(new_model, **settings["optimizer"])
+
+        # <stage continuation>
+        model, optim = new_model, new_optim
+
+        # train
+        objective = get_objective(objective_terms, settings["objective"]).to(**devtype)
+        feed = FeedWrapper(feeds[stage], max=settings["n_batches_per_epoch"], **devtype)
+
+        sched = get_scheduler(optim, **settings["lr_scheduler"])
+
+        model.train()
+        model, state, history = fit(
+            model, objective, feed, optim, sched,
+            n_epochs=settings["n_epochs"], grad_clip=settings["grad_clip"],
+            verbose=verbose)
+
+    return
 
 
 def fit(model, objective, feed, optim, sched=None, n_epochs=100,
