5835b398db79e715ee99030a54e646a64b85fea4
ivannz <ivan.nazarov@skolkovotech.ru>
2019-12-11 17:12:47 +0300

    cosmit: merged generators with their classes

--- experiments/auto/utils.py
+++ experiments/auto/utils.py
@@ -13,74 +13,48 @@ class BaseFeedWrapper(object):
     def __init__(self, feed):
         self.feed = feed
 
-    def __repr__(self):
-        return repr(self.feed)
+    def __iter__(self):
+        raise NotImplementedError
 
     def __len__(self):
         return len(self.feed)
 
-    def __iter__(self):
-        raise NotImplementedError
-
-
-def feed_mover(feed, **kwargs):
-    """Transfer to device and type cast the batches from the feed on the fly.
-
-    Parameters
-    ----------
-    feed : iterable
-        The data loader instance to draw batches from.
-
-    **kwargs : keyword arguments
-        The keyword arguments for the method `torch.Tensor.to()`.
-
-    Yields
-    ------
-    batch : iterable
-        An iterable that constitutes the batch.
-    """
-
-    if not kwargs:
-        yield from feed
-        return
-
-    for batch in feed:
-        yield [b.to(**kwargs) for b in batch]
+    def __repr__(self):
+        return f"{type(self).__name__}({repr(self.feed)})"
 
 
 class FeedMover(BaseFeedWrapper):
-    def __init__(self, feed, **kwargs):
+    def __init__(self, feed, **devtype):
         super().__init__(feed)
-        self.devtype = kwargs
+        self.devtype = devtype
 
     def __iter__(self):
-        yield from feed_mover(self.feed, **self.devtype)
+        return self.iter_impl(self.feed, **self.devtype)
 
+    @classmethod
+    def iter_impl(cls, feed, **devtype):
+        """Transfer to device and type cast the batches from the feed on the fly.
 
-def feed_limiter(feed, max=-1):
-    """Limit the number of batches requested from the feed.
+        Parameters
+        ----------
+        feed : iterable
+            The data loader instance to draw batches from.
 
-    Parameters
-    ----------
-    feed : iterable
-        The data loader instance to draw batches from.
+        **devtype : keyword arguments
+            The keyword arguments (device, dtype, ...) for the method
+            `torch.Tensor.to()`.
 
-    max : int, default=-1
-        The limit on the number of batches generated.
-        Disabled if `max` is negative.
+        Yields
+        ------
+        batch : iterable
+            An iterable that constitutes the batch.
+        """
+        if devtype:
+            for batch in feed:
+                yield [b.to(**devtype) for b in batch]
 
-    Yields
-    ------
-    batch : iterable
-        An iterable that constitutes the batch.
-    """
-
-    if max < 0:
-        yield from feed
-        return
-
-    for batch, _ in zip(feed, range(max)):
-        yield batch
+        else:
+            yield from feed
 
 
 class FeedLimiter(BaseFeedWrapper):
@@ -88,13 +62,39 @@ class FeedLimiter(BaseFeedWrapper):
         super().__init__(feed)
         self.max_iter = max_iter
 
+    def __iter__(self):
+        return self.iter_impl(self.feed, self.max_iter)
+
+    @classmethod
+    def iter_impl(cls, feed, max=-1):
+        """Limit the number of batches requested from the feed.
+
+        Parameters
+        ----------
+        feed : iterable
+            The data loader instance to draw batches from.
+
+        max : int, default=-1
+            The limit on the number of batches generated.
+            Disabled if `max` is negative.
+
+        Yields
+        ------
+        batch : iterable
+            An iterable that constitutes the batch.
+        """
+
+        if max >= 0:
+            for batch, _ in zip(feed, range(max)):
+                yield batch
+
+        else:
+            yield from feed
+
     def __len__(self):
         if self.max_iter < 0:
             return len(self.feed)
         return min(len(self.feed), self.max_iter)
-
-    def __iter__(self):
-        yield from feed_limiter(self.feed, self.max_iter)
 
 
 def feed_forward_pass(feed, module):
