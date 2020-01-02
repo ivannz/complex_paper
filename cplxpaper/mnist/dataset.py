5646f4ba72e86a3eba8f11a72f6e7c01b54b9267
Ivan Nazarov <ivannz@yandex.ru>
2020-01-02 07:38:56 +0300

    abandoned silly train-test role reversal, implemented stratified train split

--- cplxpaper/mnist/dataset.py
+++ cplxpaper/mnist/dataset.py
@@ -2,18 +2,19 @@
 
 Important
 ---------
-The roles of train and test are intentionally swapped! The reason being to
-be able to test the if the subnetwork, found by variational dropout and/or
-automatic relevance determination methods (both real and complex valued) has
-any generalization ability.
-
-PS
---
-Maybe it was better to use a small part of the official TRAIN samples?
-Without this hassle and role-switching?
+The test splits of the original datasets are returned as is to make the results
+in this paper comparable with performance reported elsewhere. However, the
+train split is made intentionally smaller in order to be able to test the if
+the sparse subnetwork, found by variational dropout and/or automatic relevance
+determination methods (both real and complex valued) keeps it generalization
+ability if at all.
 """
+import torch
 
 from torchvision import datasets, transforms
+from torch.utils.data import Subset
+
+from sklearn.model_selection import train_test_split
 
 
 class EMNIST(datasets.EMNIST):
@@ -57,106 +58,88 @@ def get_dataset(cls, root, train=True):
     ]), train=train, download=True)
 
 
-class EMNIST_Letters_Train(object):
-    """Train sample from the EMNIST letters dataset used in these experiments.
+def stratified_split(dataset, train_size=None, test_size=None, random_state=None):
+    """Random stratified train/test split."""
+    targets = dataset.targets.cpu().numpy()
+    ix_all = torch.arange(len(targets)).numpy()
 
-    Details
-    -------
-    This is actually the official `TEST` part of `EMNIST letters` dataset.
-    It is used for TRAINING intentionally.
+    # use stratified split to get the required number of samples
+    ix_train, ix_test = train_test_split(
+        ix_all, stratify=targets, train_size=train_size, test_size=test_size,
+        shuffle=True, random_state=random_state)
+
+    return Subset(dataset, ix_train), Subset(dataset, ix_test)
+
+
+def get_train_dataset(cls, root, train_size=None, random_state=None):
+    """A stratified random subset of the `train` split of the original dataset.
     """
+    dataset = get_dataset(cls, root, train=True)
+    if train_size is None:
+        return dataset
+
+    train, valid = stratified_split(dataset, train_size=train_size,
+                                    random_state=random_state)
+    return train
+
+
+class EMNIST_Letters_Train(object):
+    """Train sample from the EMNIST letters dataset."""
+
+    def __new__(cls, root, train_size=None, random_state=None):
+        return get_train_dataset(EMNIST_Letters, root, train_size=train_size,
+                                 random_state=random_state)
+
+
+class EMNIST_Letters_Test(object):
+    """Test sample from the EMNIST letters dataset."""
 
     def __new__(cls, root):
         return get_dataset(EMNIST_Letters, root, train=False)
 
 
-class EMNIST_Letters_Test(object):
-    """Test sample from the EMNIST letters dataset used in these experiments.
-
-    Details
-    -------
-    This is actually the official `TRAIN` part of `EMNIST letters` dataset.
-    It is used for TESTING intentionally.
-    """
-
-    def __new__(cls, root):
-        return get_dataset(EMNIST_Letters, root, train=True)
-
-
 class MNIST_Train(object):
-    """Train sample from the MNIST dataset used in these experiments.
+    """Train sample from the MNIST dataset."""
 
-    Details
-    -------
-    This is actually the official `TEST` part of `MNIST` dataset.
-    It is used for TRAINING intentionally.
-    """
+    def __new__(cls, root, train_size=None, random_state=None):
+        return get_train_dataset(datasets.MNIST, root, train_size=train_size,
+                                 random_state=random_state)
+
+
+class MNIST_Test(object):
+    """Test sample from the MNIST dataset."""
 
     def __new__(cls, root):
         return get_dataset(datasets.MNIST, root, train=False)
 
 
-class MNIST_Test(object):
-    """Test sample from the MNIST dataset used in these experiments.
-
-    Details
-    -------
-    This is actually the official `TRAIN` part of `MNIST` dataset.
-    It is used for TESTING intentionally.
-    """
-
-    def __new__(cls, root):
-        return get_dataset(datasets.MNIST, root, train=True)
-
-
 class KMNIST_Train(object):
-    """Train sample from the Kuzushiji-MNIST dataset used in these experiments.
+    """Train sample from the Kuzushiji-MNIST dataset.
     https://github.com/rois-codh/kmnist
-
-    Details
-    -------
-    This is actually the official `TEST` part of `KMNIST` dataset.
-    It is used for TRAINING intentionally.
     """
+
+    def __new__(cls, root, train_size=None, random_state=None):
+        return get_train_dataset(datasets.KMNIST, root, train_size=train_size,
+                                 random_state=random_state)
+
+
+class KMNIST_Test(object):
+    """Test sample from the Kuzushiji-MNIST dataset."""
 
     def __new__(cls, root):
         return get_dataset(datasets.KMNIST, root, train=False)
 
 
-class KMNIST_Test(object):
-    """Test sample from the Kuzushiji-MNIST dataset used in these experiments.
-
-    Details
-    -------
-    This is actually the official `TRAIN` part of `KMNIST` dataset.
-    It is used for TESTING intentionally.
-    """
-
-    def __new__(cls, root):
-        return get_dataset(datasets.KMNIST, root, train=True)
-
-
 class FashionMNIST_Train(object):
-    """Train sample from the Fashion-MNIST dataset used in these experiments.
+    """Train sample from the Fashion-MNIST dataset."""
 
-    Details
-    -------
-    This is actually the official `TEST` part of `Fashion-MNIST` dataset.
-    It is used for TRAINING intentionally.
-    """
-
-    def __new__(cls, root):
-        return get_dataset(datasets.FashionMNIST, root, train=False)
+    def __new__(cls, root, train_size=None, random_state=None):
+        return get_train_dataset(datasets.FashionMNIST, root, train_size=train_size,
+                                 random_state=random_state)
 
 
 class FashionMNIST_Test(object):
-    """Test sample from the Fashion-MNIST dataset used in these experiments.
-
-    Details
-    -------
-    This is actually the official `TRAIN` part of `Fashion-MNIST` dataset.
-    It is used for TESTING intentionally.
-    """
+    """Test sample from the Fashion-MNIST dataset."""
 
     def __new__(cls, root):
-        return get_dataset(datasets.FashionMNIST, root, train=True)
+        return get_dataset(datasets.FashionMNIST, root, train=False)
