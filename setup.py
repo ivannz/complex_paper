9dde9158adfe5812a566a0c32f9fc07486c63228
ivannz <ivan.nazarov@skolkovotech.ru>
2020-01-24 15:39:33 +0300

    exposed model subpackages, bumped the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="cplxpaper",
-    version="0.3",
+    version="0.3.2",
     description="""Package for Bayesian Sparsification """
                 """of Complex Valued Networks.""",
     license="MIT License",
@@ -12,7 +12,10 @@ setup(
         "cplxpaper",
         "cplxpaper.auto",
         "cplxpaper.musicnet",
+        "cplxpaper.musicnet.complex",
+        "cplxpaper.musicnet.trabelsi2017",
         "cplxpaper.mnist",
+        "cplxpaper.mnist.models",
         "cplxpaper.cifar",
         "cplxpaper.cifar.models"
     ],
