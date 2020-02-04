30d933ed3e85a68e61901cc9ece56036d504c61a
Ivan Nazarov <ivan.nazarov@skolkovotech.ru>
2020-02-05 00:48:34 +0300

    cifar10 augmented by random crop aand hor-flip

--- cplxpaper/cifar/dataset.py
+++ cplxpaper/cifar/dataset.py
@@ -51,3 +51,27 @@ class CIFAR100_Test(object):
 
     def __new__(cls, root):
         return get_dataset(datasets.CIFAR100, root, train=False)
+
+
+class AugmentedCIFAR10_Train(object):
+    """Train sample from the CIFAR10 dataset."""
+
+    def __new__(cls, root):
+        return datasets.CIFAR10(root, transform=transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ]), target_transform=None, train=True, download=True)
+
+
+class AugmentedCIFAR10_Test(object):
+    """Test sample from the CIFAR10 dataset."""
+
+    def __new__(cls, root):
+        return datasets.CIFAR10(root, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ]), target_transform=None, train=False, download=True)
