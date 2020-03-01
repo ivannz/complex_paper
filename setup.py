8f216994ee225f034f9e024053ccaf349dda4d58
ivannz <ivannz@yandex.ru>
2020-03-01 21:21:02 +0300

    changed description and bumped the version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup, Extension
 
 setup(
     name="cplxpaper",
-    version="0.4",
-    description="""Package for Bayesian Sparsification """
-                """of Complex Valued Networks.""",
+    version="0.5",
+    description="""Backend for experiments presented in Bayesian"""
+                """ Sparsification of Deep Complex-valued Networks.""",
     license="MIT License",
     author="Ivan Nazarov",
     author_email="ivan.nazarov@skolkovotech.ru",
@@ -19,7 +19,16 @@ setup(
         "cplxpaper.mnist.models",
         "cplxpaper.cifar.models"
     ],
-    requires=["torch", "numpy", "pandas", "cplxmodule", "sklearn", "ncls", "resampy", "h5py"],
+    requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "cplxmodule",
+        "sklearn",
+        "ncls",
+        "resampy",
+        "h5py"
+    ],
     # install_requires=[  # uncomment for publishing
     #     "cplxmodule @ git+https://github.com/ivannz/cplxmodule.git"
     # ]
